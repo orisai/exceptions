857b4e0f420f9c1b1ba938468220e01632714b7e
Marek Bartoš <bartos.developer152@gmail.com>
2021-07-07 17:31:00 +0200

    Suppressed exceptions

--- src/ConfigurableException.php
+++ src/ConfigurableException.php
@@ -6,12 +6,22 @@ use Exception;
 use ReflectionClass;
 use Stringable;
 use Throwable;
+use function array_key_first;
+use function array_key_last;
+use function count;
+use function explode;
+use function get_class;
+use function str_repeat;
+use function str_replace;
+use const PHP_EOL;
 
-/**
- * @mixin Exception
- */
 trait ConfigurableException
 {
+
+	/** @var array<int, Throwable> */
+	private array $suppressed = [];
+
+	private bool $isAnySuppressedInMessage = false;
 
 	/**
 	 * @return $this
@@ -31,6 +41,11 @@ trait ConfigurableException
 	{
 		$this->message = (string) $message;
 
+		$this->isAnySuppressedInMessage = false;
+		foreach ($this->suppressed as $key => $item) {
+			$this->addSuppressedToMessage($item, array_key_first($this->suppressed) === $key);
+		}
+
 		return $this;
 	}
 
@@ -46,6 +61,85 @@ trait ConfigurableException
 		$property->setAccessible(false);
 
 		return $this;
+	}
+
+	/**
+	 * @param array<Throwable> $suppressed
+	 * @return $this
+	 */
+	public function withSuppressed(array $suppressed): self
+	{
+		foreach ($suppressed as $item) {
+			$this->suppressed[] = $item;
+			$this->addSuppressedToMessage($item, count($this->suppressed) === 1);
+		}
+
+		return $this;
+	}
+
+	/**
+	 * @return array<Throwable>
+	 */
+	public function getSuppressed(): array
+	{
+		return $this->suppressed;
+	}
+
+	private function addSuppressedToMessage(Throwable $throwable, bool $isFirst): void
+	{
+		if (!$this->isAnySuppressedInMessage) {
+			$this->message .= ($this->message === '' ? '' : PHP_EOL)
+				. 'Suppressed errors:';
+		}
+
+		$this->isAnySuppressedInMessage = true;
+		$this->message .= $this->formatSuppressedExceptionMessage($throwable, $isFirst);
+	}
+
+	private function formatSuppressedExceptionMessage(Throwable $throwable, bool $isFirst): string
+	{
+		$message = $throwable->getMessage();
+		$class = get_class($throwable);
+		$file = $throwable->getFile();
+		$line = $throwable->getLine();
+
+		foreach (($trace = $throwable->getTrace()) as $key => $item) {
+			if (isset($item['class']) && $item['class'] === $class) {
+				$nextItem = $trace[$key + 1] ?? null;
+				if ($nextItem !== null && isset($nextItem['class']) && $nextItem['class'] !== $class) {
+					$file = $item['file'];
+					$line = $item['line'];
+
+					break;
+				}
+			}
+		}
+
+		$newMessage = "- $class created at $file:$line with code {$throwable->getCode()}" . PHP_EOL;
+		$newMessage .= $message === '' ? '<NO MESSAGE>' : $message;
+
+		return ($isFirst ? '' : PHP_EOL)
+			. $this->indentMessage($newMessage);
+	}
+
+	private function indentMessage(string $originalMessage): string
+	{
+		if (PHP_EOL !== "\n") {
+			$originalMessage = str_replace("\n", PHP_EOL, $originalMessage);
+		}
+
+		$message = PHP_EOL;
+		$lines = explode(PHP_EOL, $originalMessage);
+		$lastLine = array_key_last($lines);
+		$spaces = str_repeat(' ', 4);
+		foreach ($lines as $key => $line) {
+			$message .=
+				$spaces
+				. $line
+				. ($key === $lastLine ? '' : PHP_EOL);
+		}
+
+		return $message;
 	}
 
 }
--- tests/Unit/ConfigurableExceptionTest.php
+++ tests/Unit/ConfigurableExceptionTest.php
@@ -3,9 +3,12 @@
 namespace Tests\Orisai\Exceptions\Unit;
 
 use Exception;
+use Orisai\Exceptions\Logic\InvalidArgument;
 use Orisai\Exceptions\Logic\ShouldNotHappen;
 use Orisai\Exceptions\Message;
 use PHPUnit\Framework\TestCase;
+use function str_replace;
+use const DIRECTORY_SEPARATOR;
 
 final class ConfigurableExceptionTest extends TestCase
 {
@@ -35,6 +38,83 @@ final class ConfigurableExceptionTest extends TestCase
 		self::assertSame(
 			'Context: context',
 			$exception->getMessage(),
+		);
+	}
+
+	public function testSuppressed(): void
+	{
+		$suppressed = [
+			$a = new Exception('Error'),
+		];
+
+		$suppressed2 = [
+			$b = new Exception(''),
+			$c = new Exception(
+				Message::create()
+					->withProblem('problem')
+					->withSolution('solution')
+					->toString(),
+			),
+			$d = InvalidArgument::create()->withMessage('foo'),
+		];
+
+		$exception = ShouldNotHappen::create()
+			->withMessage('Oh no! This should not happen.')
+			->withSuppressed($suppressed)
+			->withSuppressed($suppressed2);
+
+		self::assertSame(
+			[$a, $b, $c, $d],
+			$exception->getSuppressed(),
+		);
+
+		self::assertSame(
+			<<<'MSG'
+Oh no! This should not happen.
+Suppressed errors:
+    - Exception created at /path/to/ConfigurableExceptionTest.php:47 with code 0
+    Error
+
+    - Exception created at /path/to/ConfigurableExceptionTest.php:51 with code 0
+    <NO MESSAGE>
+
+    - Exception created at /path/to/ConfigurableExceptionTest.php:52 with code 0
+    Problem: problem
+    Solution: solution
+
+    - Orisai\Exceptions\Logic\InvalidArgument created at /path/to/ConfigurableExceptionTest.php:58 with code 0
+    foo
+MSG,
+			str_replace(__DIR__ . DIRECTORY_SEPARATOR, '/path/to/', $exception->getMessage()),
+		);
+	}
+
+	public function testSuppressedAddedBeforeMessage(): void
+	{
+		$suppressed = new Exception('error');
+
+		$exception = ShouldNotHappen::create()
+			->withSuppressed([$suppressed]);
+
+		self::assertSame(
+			<<<'MSG'
+Suppressed errors:
+    - Exception created at /path/to/ConfigurableExceptionTest.php:94 with code 0
+    error
+MSG,
+			str_replace(__DIR__ . DIRECTORY_SEPARATOR, '/path/to/', $exception->getMessage()),
+		);
+
+		$exception->withMessage('message');
+
+		self::assertSame(
+			<<<'MSG'
+message
+Suppressed errors:
+    - Exception created at /path/to/ConfigurableExceptionTest.php:94 with code 0
+    error
+MSG,
+			str_replace(__DIR__ . DIRECTORY_SEPARATOR, '/path/to/', $exception->getMessage()),
 		);
 	}
 
